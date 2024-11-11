26a24efd1812a8a415af2cf7c2aaadd835e91832
Clansty <i@gao4.pw>
2024-11-11 21:25:37 +0800

    fix: docker run download pnpm

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,5 @@
 
 chown -R 1000:1000 /app
 
-gosu node pnpm start
+gosu node ./node_modules/.bin/prisma db push --accept-data-loss --skip-generate
+gosu node node --enable-source-maps build/index.js
